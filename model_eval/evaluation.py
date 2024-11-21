042df77073a49927c47e22eb9942e33dd7ecce2f
Ryan Deng <ryandeng@node2031.inband>
2024-11-20 20:48:29 -0500

    add in tfk client and ParEval

--- model_eval/evaluation.py
+++ model_eval/evaluation.py
@@ -1,1 +1,35 @@
+from client.driver import LLM4PP_Driver
+from client.models import LLM4PP_Problem, LLM4PP_Submission
+
+
+# import base classes for mock implementations
+from client.driver import ProblemLoader, SubmissionRunner
+from client.pareval_client import ParEvalDriver
+
+def get_mock_driver():
+    problem_loader = ProblemLoader("fake")
+    runner = SubmissionRunner()
+    return LLM4PP_Driver(problem_loader, runner)
+
+def get_pareval_driver():
+    return ParEvalDriver()
+
+# driver = get_mock_driver()
+driver = get_pareval_driver()
+
+for idx, problem in enumerate(driver):
+    problem : LLM4PP_Problem
+
+    if idx >= 2:
+        break
+
+    # do something to optimize the code.
+    # modified_source_code = f"// Optimized code!\n{problem.source_code}"
+    modified_source_code = problem.source_code
+    submission = LLM4PP_Submission(problem=problem,
+                                   submitted_code=modified_source_code)
+    driver.submit(submission)
+
+driver.save_all_responses("./tmp-mock-results.json")
+driver.evaluate()
 
